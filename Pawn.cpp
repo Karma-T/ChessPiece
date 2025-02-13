ee2a3828dcc0d32ff22e4d78503fc480be5e709b
Karma-T <148254946+Karma-T@users.noreply.github.com>
2025-02-13 10:00:55 -0500

    Update Pawn.cpp

--- Pawn.cpp
+++ Pawn.cpp
@@ -4,43 +4,21 @@
 
 #include <iostream>
 #include <string>
-#include <cctype> //isaplha and toupper functions
 #include "Pawn.hpp"
 
-using namespace std;
+// Default Constructor
+Pawn::Pawn() : ChessPiece(), double_jumpable_(false) {}
 
-Pawn::Pawn(){
-    color_ = "BLACK";
-    row_ = -1;
-    column_ = -1;
-    movingUp_ = false;
-    double_jumpable_ = false;
+// Parameterized Constructor
+Pawn::Pawn(const std::string &color, const int &row, const int &column, const bool &movingUp, const bool &double_jumpable)
+    : ChessPiece(color, row, column, movingUp), double_jumpable_(double_jumpable) {}
+
+// Accessors & Mutators
+bool Pawn::canDoubleJump() const { return double_jumpable_; }
+
+void Pawn::toggleDoubleJump() { double_jumpable_ = !double_jumpable_; }
+
+bool Pawn::canPromote() const {
+    return (movingUp_ && row_ == BOARD_LENGTH - 1) || (!movingUp_ && row_ == 0);
 }
 
-// parameterized constructor
-Pawn::Pawn(const string& color, const int& row, const int& column, const bool& movingUp, const bool& double_jumpable) : ChessPiece(color, row, column, movingUp), double_jumpable_(double_jumpable) {
-    if (!ValidColor(color)) { //if color is not valid
-        color_ = "BLACK";
-    }
-
-    if (!ValidPosition(row) || !ValidPosition(column)) { //if row and column is not valid
-        row_ = -1;
-        column_ = -1;
-    }
-}
-
-bool Pawn::canDoubleJump() const { //returns if the pawn can double jump
-    return double_jumpable_;
-}
-
-void Pawn::toggleDoubleJump() { //toggle double jump
-    double_jumpable_ = !double_jumpable_;
-}
-
-bool Pawn::canPromote() const {//returns if the pawn can promote
-    if (movingUp_) {
-        return (row_ == BOARD_LENGTH - 1); //if the pawn is on the last row
-    } else {
-        return (row_ == 0); //if the pawn is on the first row
-    }
-}
